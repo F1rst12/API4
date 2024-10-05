607cfc66ee212c3305372bb52461cc7122eaf112
tokyokitsune <147923702+tokyokitsune@users.noreply.github.com>
2024-10-05 16:03:00 +0300

    добавил защиту от ошибок

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -14,8 +14,8 @@ def fetch_nasa_epic(nasa_token):
         epic_date = nasa_image["date"]
         formatted_epic_date = datetime.datetime.fromisoformat(
             epic_date).strftime("%Y/%m/%d")
-        epic_image_url = f"https://api.nasa.gov/EPIC/archive/natural/{formatted_epic_date}/png/{epic_name}.png?api_key={nasa_token}"
-        download_image(epic_image_url, f"epic{epic_name}.png")
+        epic_image_url = f"https://api.nasa.gov/EPIC/archive/natural/{formatted_epic_date}/png/{epic_name}.png"
+        download_image(epic_image_url, f"epic{epic_name}.png", params=payload)
 
 
 def main():
